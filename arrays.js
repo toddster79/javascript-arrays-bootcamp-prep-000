783653676998eeafa612fca0bbfbe9809515fb84
Todd Carlson <todd.carlson3030@me.com>
2019-11-22 18:17:01 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,32 @@
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"
+  ]
+  
+  var array = [];
+  var newArray = [];
+  var newArray2 = [];
+  
+  function addElementToBeginningOfArray(array, element) {
+    newArray = [element, ...array];
+    return newArray;
+  }
+  
+  function destructivelyAddElementToBeginningOfArray(array, element) {
+    array.unshift(element);
+    return array;
+  }
+  
+  function addElementToEndOfArray(array, element) {
+    newArray2 = [...array, element];
+    return newArray2;
+  }
+  
+  function destructivelyAddElementToEndOfArray(array, element) {
+    array.push(element);
+    return array;
+  }
+  
+  
